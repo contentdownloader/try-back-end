cd86fb43d053848534ab2b0c01906620578d719a
contentdownloader <t.change@mail.ru>
2025-06-22 19:46:31 +0200

    Create testContent.js

---
+++ services/testContent.js
@@ -0,0 +1,8 @@
+export async function downloadTestContent(url, contentType, downloadDir) {
+  // Simulate a file download for testing
+  return {
+    success: true,
+    message: 'Test content downloaded successfully',
+    filename: 'test_file.txt'
+  };
+}
